16fe9987aea1fe48f588428f9bedf99eaf54b64f
AphiwatJs <aphiwat.jantharasrisao@gmail.com>
2024-07-02 10:47:25 +0700

    not-found middlewares

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const notFound = require('./middlewares/not-found')
 const app = express()
 
 const port = process.env.PORT || 8000
+app.use(notFound)
+
 app.listen(port, ()=>console.log('server running',port))
