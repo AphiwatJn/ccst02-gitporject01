a8601ff0cac3278795a4859f4f56a9443771bf83
AphiwatJs <aphiwat.jantharasrisao@gmail.com>
2024-07-02 11:28:56 +0700

    todoRoute

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ require('dotenv').config()
 const notFound = require('./middlewares/not-found')
 const errorMw = require('./middlewares/error-mw')
 const app = express()
+const todoRoute = require('./routes/todo-route')
 
-const port = process.env.PORT || 8000
+
+app.use('/todo',todoRoute)
+const port = process.env.PORT || 9999
 app.use(notFound)
 app.use(errorMw)
 
