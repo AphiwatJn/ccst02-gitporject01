403ce35e81333ae6c7e5d46320bb11a2e762fc4b
AphiwatJs <aphiwat.jantharasrisao@gmail.com>
2024-07-02 11:10:36 +0700

    error mw

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const notFound = require('./middlewares/not-found')
+const errorMw = require('./middlewares/error-mw')
 const app = express()
 
 const port = process.env.PORT || 8000
 app.use(notFound)
+app.use(errorMw)
 
 app.listen(port, ()=>console.log('server running',port))
